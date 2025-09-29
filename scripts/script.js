e1bfc81f0b639e05f304afa61ff83a0776eae518
Matheo Duhamel <Matheo.duhamel@etu.unicaen.fr>
2025-09-29 09:01:59 +0200

    [ADD] implementation API

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-const API_KEY = "6f464de156msh2e28f7c658a93c0p103939jsnb8535fa80c55"
+const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '6f464de156msh2e28f7c658a93c0p103939jsnb8535fa80c55',
+		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

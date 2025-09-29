20a88dced10af5dda6d2b79326722ac048fa70ec
alexandre <alexandre.lenormand@etu.unicaen.fr>
2025-09-29 09:26:58 +0200

    [ADD] affichage console data

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,34 @@ const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmet
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '6f464de156msh2e28f7c658a93c0p103939jsnb8535fa80c55',
+		'x-rapidapi-key': '6086c83303msha21ced5cae2b469p13aae7jsn8eac00c91c09',
 		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+
+
+
+async function fetchAndDisplayAnime() {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json(); 
+        // Vérifie si des résultats existent
+        if (data.data && data.data.length > 0) {
+            data.data.forEach(anime => {
+                console.log(`Titre : ${anime.title}`);
+                console.log(`Synopsis : ${anime.synopsis}`);
+                console.log(`catégories : ${anime.genres}`)
+                console.log(`Classement : ${anime.ranking}`);
+                console.log(`Nombre d'épisodes : ${anime.episodes}`);
+                console.log('-----------------------------');
+            });
+        } else {
+            console.log('Aucun anime trouvé.');
+        }
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+    }
 }
+
+fetchAndDisplayAnime();

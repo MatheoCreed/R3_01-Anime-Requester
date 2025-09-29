4e87e56fec790120ca8839e00abc0a9c6208d079
TitouSou <titisou14@outlook.fr>
2025-09-29 11:39:09 +0200

    [FIX + ADD] affichage + utilisation API

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
-fetch("../animes.json")
+fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "anime-db.p.rapidapi.com",
+        "x-rapidapi-key": "136c0c4dcdmsh920a3205050b6d3p164bb6jsn5e0e89c2aad1" // ⚠️ ta clé
+      }
+    })
     .then(response => response.json())
     .then(data => {
-        const animes = data.animes;
 
-        animes.forEach(anime => {
+        data.data.forEach(anime => {
             const card = document.createElement("div");
             card.className = "card";
 
@@ -11,6 +16,7 @@ fetch("../animes.json")
                 <h3>${anime.title}</h3>
                 <img src="${anime.image}" alt="${anime.title}">
                 <p>${anime.synopsis}</p>
+                <p>Genre : ${anime.genre}</p>
             `;
 
             document.body.appendChild(card);

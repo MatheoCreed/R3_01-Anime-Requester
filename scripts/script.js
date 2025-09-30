6b2bbe51922897baaa7ef8aadab451bb849711e5
alexandre <alexandre.lenormand@etu.unicaen.fr>
2025-09-30 11:41:12 +0200

    [FIX] affichage + responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,27 @@
 fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc", {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "anime-db.p.rapidapi.com",
-        "x-rapidapi-key": "136c0c4dcdmsh920a3205050b6d3p164bb6jsn5e0e89c2aad1" // ⚠️ ta clé
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "anime-db.p.rapidapi.com",
+    "x-rapidapi-key": "6086c83303msha21ced5cae2b469p13aae7jsn8eac00c91c09" 
+  }
+})
+.then(response => response.json())
+.then(data => {
+    const container = document.getElementById("results")
 
-        data.data.forEach(anime => {
-            const card = document.createElement("div");
-            card.className = "card";
+    data.data.forEach(anime => {
+        const card = document.createElement("div");
+        card.className = "card";
 
-            card.innerHTML = `
-                <h3>${anime.title}</h3>
-                <img src="${anime.image}" alt="${anime.title}">
-                <p>${anime.synopsis}</p>
-                <p>Genre : ${anime.genres.join(", ")}</p>
-                <p>Classement : ${anime.ranking}</p>
-                <p>Episodes : ${anime.episodes}</p>
-            `;
+        card.innerHTML = `
+            <h3>${anime.title}</h3>
+            <img src="${anime.image}" alt="${anime.title}">
+            <p>${anime.synopsis}</p>
+            <p>Genre : ${anime.genres.join(", ")}</p>
+            <p>Classement : ${anime.ranking}</p>
+            <p>Episodes : ${anime.episodes}</p>
+        `;
 
-            document.body.appendChild(card);
-        });
-    })
+        container.appendChild(card);
+    });
+})
